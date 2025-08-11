acd2e5a95f90f3fbfee5280584c1f4451d769765
maxitabakman <49928297@est.ort.edu.ar>
2025-08-11 15:47:46 -0300

    cambio de contraseña

---
+++ front/main js front.js
@@ -0,0 +1,10 @@
+function cambiardatosdelusuario() {
+    const inputElement = document.getElementById("contraseña");
+    const valorDelInput = inputElement.value;
+    let contrseña = valorDelInput;
+    console.log(contrseña);
+    const inputElement2 = document.getElementById("sesion");
+    const secion = inputElement2.value;
+    let sesion = secion;
+    console.log(sesion);
+  }
