3b93f082eba46edd3b4724852bab771acd21932e
maxitabakman <49928297@est.ort.edu.ar>
2025-08-13 11:37:58 -0300

    arregle un par de cosas

--- front/main js front.js
+++ front/main js front.js
@@ -2,13 +2,13 @@ function cambiardatosdelusuario() {
     const inputcontraseña = document.getElementById("contraseña");
     const valorinputcontraseña = inputcontraseña.value;
     let contrseña = valorinputcontraseña;
-    console.log(contrseña);
+    console.log("contraseña:" + contrseña);
     const inputgmail = document.getElementById("gmail");
     const valorinputgmail = inputgmail.value;
     let gmail = valorinputgmail;
-    console.log(gmail);
+    console.log( "correo electronico:" +gmail);
     const inputratioinsulina= document.getElementById("ratioinsulina");
     const  valorinputratioinsulina= inputratioinsulina.value;
     let ratioinsulina = valorinputratioinsulina;
-    console.log(ratioinsulina);
-  }
+    console.log("ratio de insulina:" + ratioinsulina);
+}
