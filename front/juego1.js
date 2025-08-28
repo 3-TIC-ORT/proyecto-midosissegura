2bfa88666c92dff77d26bf35fca70954d34e0a9a
malerossi <malena.agostina.rossi@gmail.com>
2025-08-28 10:36:42 -0300

    empece juego 1

---
+++ front/juego1.js
@@ -0,0 +1,14 @@
+const paso1 = document.getElementById ("PASO 1");
+const paso2 = document.getElementById ("PASO 2");
+const paso3 = document.getElementById ("PASO 3");
+const paso4 = document.getElementById ("PASO 4");
+const paso5 = document.getElementById ("PASO 5");
+const paso6 = document.getElementById ("PASO 6");
+const paso7 = document.getElementById ("PASO 7");
+const paso8 = document.getElementById ("PASO 8");
+
+function click(event) {
+    const paso1 = event.target.id;
+}
+paso1.addEventListener ("click", click);
+paso2.addEventListener ("click", click);
