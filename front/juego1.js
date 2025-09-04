55edd6295aaf069c9d2d896d08b539ec57480843
malerossi <malena.agostina.rossi@gmail.com>
2025-09-04 10:40:13 -0300

    termien CREO

--- front/juego1.js
+++ front/juego1.js
@@ -1,23 +1,32 @@
-const paso1 = document.getElementById ("PASO 1");
-const paso2 = document.getElementById ("PASO 2");
-const paso3 = document.getElementById ("PASO 3");
-const paso4 = document.getElementById ("PASO 4");
-const paso5 = document.getElementById ("PASO 5");
-const paso6 = document.getElementById ("PASO 6");
-const paso7 = document.getElementById ("PASO 7");
+const paso1 = document.getElementById ("paso1");
+const paso2 = document.getElementById ("paso2");
+const paso3 = document.getElementById ("paso3");
+const paso4 = document.getElementById ("paso4");
+const paso5 = document.getElementById ("paso5");
+const paso6 = document.getElementById ("paso6");
+const paso7 = document.getElementById ("paso7");
 
 const InstruccionesPopUp = {
-    "PASO 1": "Primero, lava tus manos con agua y jabón para que estén limpias.",
-    "PASO 2": "Mira que la lapicera tenga la medicina correcta y esté lista para usar.",
-    "PASO 3": "Coloca una aguja nueva en la lapicera, como ponerle un lápiz a la punta.",
-    "PASO 4": "Gira la ruedita y aprieta el botón para sacar un poco de aire, así la medicina sale bien.",
-    "PASO 5": "Gira la ruedita hasta el número que te dijo el doctor.",
-    "PASO 6": "Pincha un poquito en tu barriguita o pierna y aprieta el botón para que entre la medicina.",
-    "PASO 7": "Saca la aguja con cuidado y ponla en un bote especial para que nadie se lastime.",
+    "paso1": "Primero, lava tus manos con agua y jabón para que estén limpias.",
+    "paso2": "Mira que la lapicera tenga la medicina correcta y esté lista para usar.",
+    "paso3": "Coloca una aguja nueva en la lapicera, como ponerle un lápiz a la punta.",
+    "paso4": "Gira la ruedita y aprieta el botón para sacar un poco de aire, así la medicina sale bien.",
+    "paso5": "Gira la ruedita hasta el número que te dijo el doctor.",
+    "paso6": "Pincha un poquito en tu barriguita o pierna y aprieta el botón para que entre la medicina.",
+    "paso7": "Saca la aguja con cuidado y ponla en un bote especial para que nadie se lastime.",
 }
 
 function Instrucciones (event) {
     const paso = event.target.id;
     const popup = InstruccionesPopUp [paso];
     alert (popup)
+
+    document.getElementById (popup).addEventListener ("click", cerrarPopup)
 }
+paso1.addEventListener("click", Instrucciones);
+paso2.addEventListener("click", Instrucciones);
+paso3.addEventListener("click", Instrucciones);
+paso4.addEventListener("click", Instrucciones);
+paso5.addEventListener("click", Instrucciones);
+paso6.addEventListener("click", Instrucciones);
+paso7.addEventListener("click", Instrucciones);
