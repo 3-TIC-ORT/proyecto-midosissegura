e25df3a3dc065838568ef4b694792b842ff3dbc7
maximiliano tabakman <tabakmanmaxi@gmail.com>
2025-09-09 00:40:41 -0300

    casi termino el juego 3

--- front/juego3.js
+++ front/juego3.js
@@ -1,14 +1,29 @@
-
 const botonorden1 = document.getElementById("boton1");
 const botonorden2 = document.getElementById("boton2");
+const botonorden3 = document.getElementById("boton3");
 const img1 = document.getElementById("imagen1");
 const img2 = document.getElementById("imagen2");
 const img3 = document.getElementById("imagen3");
-botonorden1.addEventListener("click", () => {
+
+let imagenSeleccionada =null;
+
 img1.addEventListener("click", () => {
-  img1.classList.add("orden-1")
-})});
-botonorden2.addEventListener("click", () => {
-  img2.addEventListener("click", () => {
-    img2.classList.add("orden2")
-  })});
+  imagenSeleccionada = img1.getAttribute("src");
+});
+img2.addEventListener("click", () => {
+  imagenSeleccionada = img2.getAttribute("src");
+});
+img3.addEventListener("click", () => {
+  imagenSeleccionada = img3.getAttribute("src");
+});
+
+function aplicarFondo(boton) {
+  if (imagenSeleccionada) {
+    boton.style.backgroundImage = `url("${imagenSeleccionada}")`;
+    boton.style.backgroundSize = "cover";  
+    boton.style.backgroundPosition = "center";
+  }
+}
+botonorden1.addEventListener("click", () => aplicarFondo(botonorden1));
+botonorden2.addEventListener("click", () => aplicarFondo(botonorden2));
+botonorden3.addEventListener("click", () => aplicarFondo(botonorden3));
