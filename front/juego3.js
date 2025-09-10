b1ccd9496d86557d32925e4d0250578897232d39
maximiliano tabakman <tabakmanmaxi@gmail.com>
2025-09-10 00:17:21 -0300

    termine el juego 3

--- front/juego3.js
+++ front/juego3.js
@@ -1,9 +1,17 @@
 const botonorden1 = document.getElementById("boton1");
 const botonorden2 = document.getElementById("boton2");
 const botonorden3 = document.getElementById("boton3");
+const botonorden4 = document.getElementById("boton4");
+const botonorden5 = document.getElementById("boton5");
+const botonorden6 = document.getElementById("boton6");
+const botonorden7 = document.getElementById("boton7");
 const img1 = document.getElementById("imagen1");
 const img2 = document.getElementById("imagen2");
 const img3 = document.getElementById("imagen3");
+  const img4 = document.getElementById("imagen4");
+  const img5 = document.getElementById("imagen5");
+  const img6 = document.getElementById("imagen6");
+  const img7 = document.getElementById("imagen7");
 
 let imagenSeleccionada =null;
 
@@ -17,6 +25,19 @@ img3.addEventListener("click", () => {
   imagenSeleccionada = img3.getAttribute("src");
 });
 
+img4.addEventListener("click", () => {
+  imagenSeleccionada = img4.getAttribute("src");
+});
+img5.addEventListener("click", () => {
+  imagenSeleccionada = img5.getAttribute("src");
+});
+img6.addEventListener("click", () => {
+  imagenSeleccionada = img6.getAttribute("src");
+});
+img7.addEventListener("click", () => {
+  imagenSeleccionada = img7.getAttribute("src");
+});
+
 function aplicarFondo(boton) {
   if (imagenSeleccionada) {
     boton.style.backgroundImage = `url("${imagenSeleccionada}")`;
@@ -27,3 +48,7 @@ function aplicarFondo(boton) {
 botonorden1.addEventListener("click", () => aplicarFondo(botonorden1));
 botonorden2.addEventListener("click", () => aplicarFondo(botonorden2));
 botonorden3.addEventListener("click", () => aplicarFondo(botonorden3));
+botonorden4.addEventListener("click", () => aplicarFondo(botonorden4));
+botonorden5.addEventListener("click", () => aplicarFondo(botonorden5));
+botonorden6.addEventListener("click", () => aplicarFondo(botonorden6));
+botonorden7.addEventListener("click", () => aplicarFondo(botonorden7));
