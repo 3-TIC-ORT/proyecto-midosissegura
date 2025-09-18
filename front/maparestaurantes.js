d186b55a94b0b2c9a7744c5a3a23b3ffbd22da7d
malerossi <malena.agostina.rossi@gmail.com>
2025-09-18 12:02:44 -0300

    va queriendo

--- front/maparestaurantes.js
+++ front/maparestaurantes.js
@@ -4,6 +4,14 @@ const restaurante3 = document.getElementById ("restaurante3");
 const restaurante4 = document.getElementById ("restaurante4");
 const restaurante5 = document.getElementById ("restaurante5");
 
+const InformacionMapa = {
+    "restaurante1" : "Sintaxis, ubicado en Av. Cabildo 2607, Belgrano, CABA, es un restaurante donde preparan comidas que ayudan a cuidar el azúcar en tu cuerpo. Tienen platos ricos y saludables para que los niños se sientan con energía para jugar y aprender asi como nuggets de pollo caseros con verduras al vapor, puré de calabaza, y gelatina de frutas sin azúcar."
+    "restaurante2" : "Cocelia, ubicado en Av. Libertador 8000, Nuñez, CABA, es un restaurante que ofrece comidas sanas y sabrosas para que los niños crezcan fuertes y felices, asi como mini milanesas de pollo al horno con ensalada colorida, sopa de verduras casera y fruta fresca picada"
+    "restaurante3" : ""
+    "restaurante4" : ""
+    "restaurante5" : ""
+}
+
 function mapa (event) {
     const mapa = event.target.id;
     const popup = InstruccionesPopUp [mapa];
