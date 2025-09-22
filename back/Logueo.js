ac65f756adda43caa4dcfaf7a45fa0e1915a7a9a
MartoMorales <martin.morales.larrozag@gmail.com>
2025-09-22 12:04:09 -0300

    DIOS ME AMPARE

--- back/Logueo.js
+++ back/Logueo.js
@@ -1,44 +1,45 @@
 import fs from "fs";
 
-var packagejson = JSON.parse(fs.readFileSync('Usuarios.json', 'utf-8'));
-var Usuario = packagejson.Usuarios
-console.log(Usuario)
 
-var registro=[
-    {
-        NOMBRE:"",
-        CONTRASEÑA:"",
-        GMAIL:"",
-        APELLIDO:""
-    }
-]
+var registro= JSON.parse(fs.readFileSync("Usuarios.json", "utf-8"))
+console.log(registro)
+
 
 
 function Iniciar()
 {
-    var nombre = document.getElementById('nombre').value
-    var contraseña = document.getElementById('contraseña').value
-    var gmail = document.getElementById('gmail').value
-    var apellido = document.getElementById('apellido').value
+  //  var nombre = document.getElementById('nombre').value
+  // var contraseña = document.getElementById('contraseña').value
+  //  var gmail = document.getElementById('gmail').value
+  //  var apellido = document.getElementById('apellido').value
 
-    for (var i =0; i<Usuario.length;i++)
+    var nombre = "Marto"
+    var apellido= "Morales"
+    var contraseña = "Martito123"
+    var gmail = "Martin@gmail.com"
+
+
+    for (var i =0; i<registro.length;i++)
     {
-        if (gmail == Usuario[i].gmail)
+        if (gmail == registro[i].gmail)
         {
             alert ("Este correo ya está siendo utilizado por otra cuenta")
+            var serepite = true
         }
-
-        else {
+        
+    }
+        if (serepite != true)
+            {
             registro.push ({
-                NOMBRE:nombre,
-                CONTRASEÑA:contraseña,
-                GMAIL:gmail,
-                APELLIDO:apellido
+                "NOMBRE":'"' + nombre + '"',
+                "APELLIDO":'"' + apellido + '"',
+                "GMAIL":'"' + gmail + '"',
+                "CONTRASEÑA":'"' + contraseña + '"'
             })
             fs.writeFileSync('Usuarios.json', JSON.stringify(registro))
             console.log("Usuario registrado con éxito")
         }
-    }
 }
 
+Iniciar()
 
