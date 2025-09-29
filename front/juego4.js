12fbfe0f2cda60fc272b392ea8c913aa78a09720
malerossi <malena.agostina.rossi@gmail.com>
2025-09-29 10:40:04 -0300

    no lo termine creo, pero casi

--- front/juego4.js
+++ front/juego4.js
@@ -1,10 +1,19 @@
 let nivelglucemia= 70;
 const plato= document.getElementById ("plato");
+const platofideos = document.getElementById ("Fideos");
+const platopapasfritas = document.getElementById ("PapasFritas");
+const platocarne = document.getElementById ("Carne");
+const platohelado = document.getElementById ("Helado");
+
+function cambiosglucemia (cambio) {
+    nivelglucemia += cambio
+}
 
 const platos= [
     "img/plato1.png",
     "img/plato2.png",
-    "img/plato3.png"
+    "img/plato3.png",
+    "img/plato4.png"
 ];
 
 function platoaleatorio () {
@@ -12,6 +21,22 @@ function platoaleatorio () {
     plato.src =  platos [platorandom];
 }
 
-function medidor() {
-    const glucemia= 
-}
+platofideos.addEventListener ("click", function() {
+    cambiosglucemia (50);
+    alert ("La glucemia esta a " + nivelglucemia);
+})
+
+platopapasfritas.addEventListener ("click", function() {
+    cambiosglucemia (40);
+    alert ("La glucemia esta a " + nivelglucemia);
+})
+
+platocarne.addEventListener ("click", function() {
+    cambiosglucemia (10);
+    alert ("La glucemia esta a " + nivelglucemia);
+})
+
+platohelado.addEventListener ("click", function() {
+    cambiosglucemia (60);
+    alert ("La glucemia esta a " + nivelglucemia);
+})
