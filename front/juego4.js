f4edbad2df0759dd66d951fa9c366d85b2a6d84f
malerossi <malena.agostina.rossi@gmail.com>
2025-09-29 15:54:42 -0300

    crisis juego 4

--- front/juego4.js
+++ front/juego4.js
@@ -4,21 +4,27 @@ const platofideos = document.getElementById ("Fideos");
 const platopapasfritas = document.getElementById ("PapasFritas");
 const platocarne = document.getElementById ("Carne");
 const platohelado = document.getElementById ("Helado");
+const moguls = document.getElementById ("Moguls");
+const lapicera = document.getElementById ("Lapicera");
 
 function cambiosglucemia (cambio) {
     nivelglucemia += cambio
 }
 
 const platos= [
-    "img/plato1.png",
-    "img/plato2.png",
-    "img/plato3.png",
-    "img/plato4.png"
+    "fideos.jpg",
+    "carneeeee.jfif",
+    "helado.jpg",
+    "papas.jfif"
 ];
 
 function platoaleatorio () {
     const platorandom= Math.floor(Math.random() * platos.length);
     plato.src =  platos [platorandom];
+}
+
+window.onload = function () {
+    platoaleatorio ();
 }
 
 platofideos.addEventListener ("click", function() {
@@ -38,5 +44,15 @@ platocarne.addEventListener ("click", function() {
 
 platohelado.addEventListener ("click", function() {
     cambiosglucemia (60);
+    alert ("La glucemia esta a " + nivelglucemia);
+})
+
+moguls.addEventListener ("click", function() {
+    cambiosglucemia (15);
+    alert ("La glucemia esta a " + nivelglucemia);
+})
+
+lapicera.addEventListener ("click", function() {
+    cambiosglucemia (-30);
     alert ("La glucemia esta a " + nivelglucemia);
 })
