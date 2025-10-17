6a72924ba437a442438046dbbb9b243fd55d0db7
malerossi <malena.agostina.rossi@gmail.com>
2025-10-17 01:11:59 -0300

    TAN CERCA PERO TAN LEJOS

--- front/INICIO DE SESION/iniciodesesion.js
+++ front/INICIO DE SESION/iniciodesesion.js
@@ -1,11 +1,34 @@
+const nombreniño = document.getElementById ("nombrenene");
+const apellidoniño = document.getElementById ("apellidonene");
+const botonEnviar = document.getElementById ("send")
+
+botonEnviar.addEventListener ("click", function(){
+alert ("Bienvenido " + nombreniño.value + " " + apellidoniño.value);
+})
+
 let cabello = 0;
 const cabellos = document.querySelectorAll ('#cabellos .pelo')
 
-cabellos.forEach (img => {
-    img.addEventListener ("click", () => {
+document.getElementById ('cabellos').addEventListener ("click", () => {
         cabellos[cabello].classList.remove ('visible');
         cabello = (cabello + 1) % cabellos.length;
         cabellos[cabello].classList.add ('visible');
-    })
 })
 
+let lentes = 0;
+const anteojos = document.querySelectorAll ('#anteojosysol .lentes')
+
+document.getElementById ('anteojosysol').addEventListener ("click", () => {
+        anteojos[lentes].classList.remove ('visible');
+        lentes = (lentes + 1) % anteojos.length;
+        anteojos[lentes].classList.add ('visible');
+})
+
+let piel = 0;
+const pieles = document.querySelectorAll ('#colorcitos .piel')
+
+document.getElementById ('colorcitos').addEventListener ("click", () => {
+        pieles[piel].classList.remove ('visible');
+        piel = (piel + 1) % pieles.length;
+        pieles[piel].classList.add ('visible');
+})
