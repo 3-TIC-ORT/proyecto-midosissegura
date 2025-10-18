0add4898d3db295ff0c2d48f60e8e15b540c9e1a
malerossi <malena.agostina.rossi@gmail.com>
2025-10-18 03:01:49 -0300

    ahre que terminos son solo 10 lineas

---
+++ front/terminosycondiciones/terminos.js
@@ -0,0 +1,10 @@
+const acepta = document.getElementById ("si");
+const rechaza = document.getElementById ("no");
+
+acepta.addEventListener ("click", function() {
+    alert ("Aceptaste");
+})
+
+rechaza.addEventListener ("click", function() {
+    alert ("Rechazaste");
+})
