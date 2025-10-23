9885ac51daeb0167d087af8aa0e718b8a5865ef5
malerossi <malena.agostina.rossi@gmail.com>
2025-10-22 22:27:57 -0300

    wtf este juego SOY MALISIMA CON CSS PORFAVOR AYUDA

---
+++ front/glucagon/glucagon.js
@@ -0,0 +1,7 @@
+const lineas = document.getElementById ("lineas");
+const ctx = lineas.getContext ("2d");
+let azul = null;
+
+const botonesazules = document.querySelectorAll ("botonesazules");
+const botonesrojos = document.querySelectorAll ("botonesrojos");
+
