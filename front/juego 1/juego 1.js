039a8a46b93131150c0a91ff9e0466eef110a98a
maximiliano tabakman <tabakmanmaxi@gmail.com>
2025-10-26 01:39:47 -0300

    estoy mal de la cabeza

--- front/juego 1/juego 1.js
+++ front/juego 1/juego 1.js
@@ -6,11 +6,12 @@ let img2 = document.getElementById ("img2");
 let img3 = document.getElementById ("img3");
 const botonesazules = document.querySelectorAll ("botonesazules");
 const botonesrojos = document.querySelectorAll ("botonesrojos");
+const prueba1 = document.getElementById ("prueba1");
+const prueba2 = document.getElementById ("prueba2");
 let numero = 0;
 let imgactiva = null;
 const uno = "posicion1";
 function cambiarnm (posicion){
-
 numero= Math.random ();
 if (numero <=0.33){
 img1.classList.add ("display");
@@ -20,13 +21,22 @@ imgactiva = "img1";
 else if (numero >0.33 && numero <=0.66){
 img2.classList.add ("display");
 img2.classList.add (posicion);
-imgactiva = "img2"
-;
+imgactiva = "img2";
 }
 else {
 img3.classList.add ("display");
 img3.classList.add (posicion);
 imgactiva = "img3"
 }
+if (imgactiva == "img1"){
+prueba1.innerHTML = "tostada";
+prueba2.innerHTML = "carne y banana";}
+else if (imgactiva == "img2"){
+prueba1.innerHTML = "carne";
+prueba2.innerHTML = "tostada y banana";
 }
+else{
+prueba1.innerHTML = "banana";
+prueba2.innerHTML = "tostada y carne";
+}}
 cambiarnm (uno);
