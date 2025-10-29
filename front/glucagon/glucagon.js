7f7a36ba6c1cfc54a7df95bf7290d52ac7563178
malerossi <malena.agostina.rossi@gmail.com>
2025-10-29 10:42:38 -0300

    problema: es muy raro la agrupacion en figma yy...

---
+++ front/glucagon/glucagon.js
@@ -0,0 +1,32 @@
+let uno = document.getElementById ("uno");
+let dos = document.getElementById ("dos");
+dos.classList.add ("display");
+let tres = document.getElementById ("tres");
+tres.classList.add ("display");
+let cuatro = document.getElementById ("cuatro");
+cuatro.classList.add ("display");
+let cinco = document.getElementById ("cinco");
+cinco.classList.add ("display");
+let seis = document.getElementById ("seis");
+seis.classList.add ("display");
+let siete = document.getElementById ("siete");
+siete.classList.add ("display");
+let ocho = document.getElementById ("ocho");
+ocho.classList.add ("display");
+let nueve = document.getElementById ("nueve");
+nueve.classList.add ("display");
+let diez = document.getElementById ("diez");
+diez.classList.add ("display");
+let once = document.getElementById ("once");
+once.classList.add ("display");
+let doce = document.getElementById ("doce");
+doce.classList.add ("display");
+let trece = document.getElementById ("trece");
+trece.classList.add ("display");
+let catorce = document.getElementById ("catorce");
+catorce.classList.add ("display");
+
+let atras = document.getElementById ("atras");
+let seguir = document.getElementById ("seguir");
+
+atras.addEventListener ("click")
