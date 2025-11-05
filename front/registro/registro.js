5b33cc3412692e355ca21bbe8bd9d5e31dc6d336
malerossi <malena.agostina.rossi@gmail.com>
2025-11-05 10:36:25 -0300

    qu ecraclek registro sin soquetic ni localstorage

---
+++ front/registro/registro.js
@@ -0,0 +1,104 @@
+connect2Server ();
+
+const cabellos = document.getElementById ("cabellos");
+
+const NADA = document.getElementById ("NADA");
+const PELADO = document.getElementById ("PELADO");
+const VERDO = document.getElementById ("VERDO");
+const RUBIO = document.getElementById ("RUBIO");
+const ROJO = document.getElementById ("ROJO");
+const AZUL = document.getElementById ("AZUL");
+const CELESTO = document.getElementById ("CELESTO");
+const PELINEGRO = document.getElementById ("PELINEGRO");
+const MOROCHO = document.getElementById ("MOROCHO");
+const VERDA = document.getElementById ("VERDA");
+const RUBIA = document.getElementById ("RUBIA");
+const ROJA = document.getElementById ("ROJA");
+const AZULA = document.getElementById ("AZULA");
+const CELESTA = document.getElementById ("CELESTA");
+const PELINEGRA = document.getElementById ("PELINEGRA");
+const MOROCHA = document.getElementById ("MOROCHA");
+const ALBINO = document.getElementById ("ALBINO");
+
+const pelos = [
+    NADA,
+    PELADO,
+    VERDO,
+    RUBIO,
+    ROJO,
+    AZUL,
+    CELESTO,
+    PELINEGRO,
+    MOROCHO,
+    VERDA,
+    RUBIA,
+    ROJA,
+    AZULA,
+    CELESTA,
+    PELINEGRA,
+    MOROCHA,
+    ALBINO
+]
+
+const cabelloenpantalla = [
+function cabelloenpantalla() {
+    return pelos.find((cabellos) => cabellos.style.display === "inline");
+  }]
+
+const anteojosysol = document.getElementById  ("anteojosysol");
+
+const NADASOL = document.getElementById ("NADA");
+const SOL = document.getElementById ("SOL");
+const MIOPE = document.getElementById ("MIOPE");
+
+const tiposanteojosysol = [
+    NADASOL,
+    SOL,
+    MIOPE
+]
+
+const anteojosenpantalla = [
+    function anteojosenpantalla() {
+        return tiposanteojosysol.find((anteojosysol) => anteojosysol.style.display === "inline");
+      }]
+
+
+const colorcitos = document.getElementById ("cabellos");
+
+const NADAENTERO = document.getElementById ("NADA");
+const NORMALENTERO = document.getElementById ("NORMALENTERO");
+const NEGROENTERO = document.getElementById ("NEGROENTERO");
+const ALBINOENTERO = document.getElementById ("ALBINOENTERO");
+      
+const piel = [
+    NADAENTERO,
+    NORMALENTERO,
+    NEGROENTERO,
+    ALBINOENTERO
+]
+      
+const pielenpantalla = [
+    function pielenpantalla() {
+          return piel.find((colorcitos) => colorcitos.style.display === "inline");
+        }]
+
+const dataavatar = [
+    cabelloenpantalla,
+    anteojosenpantalla,
+    pielenpantalla
+]
+
+function enviardatos() {
+    postEvent("NiñoRegistrado", {
+        NOMBRE: localStorage.getItem("nombreniño"),
+        APELLIDO: localStorage.getItem("apellidoniño"),
+        comida: comidaelegida.value,
+        CANTIDAD: cantidades.value,
+    },
+    function (data) {
+      const respuesta = data;
+      alert(`Calorías: ${respuesta} Kcal`);
+      resultado.innerText = `Calorías: ${respuesta} Kcal`;
+    });
+}
+    
