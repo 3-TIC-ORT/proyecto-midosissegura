cc2f795285affd61d85c02eeefde074e98c192fa
Su Nombre <malena.agostina.rossi@gmail.com>
2025-11-09 18:50:45 -0300

    pero lpm DONDE ESTA RATIO Y EDAD

--- front/usuarioniño/usuarioniño.js
+++ front/usuarioniño/usuarioniño.js
@@ -1,1 +1,16 @@
 connect2Server();
+
+function datosUsuario() {
+    NOMBREyAPELLIDO = localStorage.getItem("nombreniño") + " " + localStorage.getItem("apellidoniño");
+    RATIO = "";
+    EDAD = "";
+    TROFEOS = "";
+
+    document.getElementById("nombreyapellido").textContent = NOMBREyAPELLIDO;
+    document.getElementById("ratio").textContent = RATIO;
+    document.getElementById("edad").textContent = EDAD;
+    document.getElementById("trofeos").textContent = TROFEOS;
+}
+
+datosUsuario();
+
