20e5951e56b24b74f8cd7b11884af8586be85ca5
malerossi <malena.agostina.rossi@gmail.com>
2025-11-13 22:59:36 -0300

    que

--- front/usuarioniño/usuarioniño.js
+++ front/usuarioniño/usuarioniño.js
@@ -4,9 +4,34 @@ let nombre = localStorage.getItem("nombreniño");
 let apellido = localStorage.getItem("apellidoniño");
 let ratio = 0;
 let TROFEOS = 0;
+let T1 = document.getElementById ("T1");
+let T2 = document.getElementById ("T2");
+let T3 = document.getElementById ("T3");
+let T4 = document.getElementById ("T4");
+let T5 = document.getElementById ("T5");
+let T6 = document.getElementById ("T6");
 let valorpiel = 0;
 let valorpelo = 0;
 let valoranteojos = 0;
+
+function ocultartrofeos (){
+  T1.style.display = "none";
+  T2.style.display = "none";
+  T3.style.display = "none";
+  T4.style.display = "none";
+  T5.style.display = "none";
+  T6.style.display = "none";
+}
+ocultartrofeos ();
+
+function mostrartrofeos (t1, t2, t3, t4, t5, t6){
+  if (t1 == 1) T1.style.display = "block";
+  if (t2 == 1) T2.style.display = "block";
+  if (t3 == 1) T3.style.display = "block";
+  if (t4 == 1) T4.style.display = "block";
+  if (t5 == 1) T5.style.display = "block";
+  if (t6 == 1) T6.style.display = "block";
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   valorpiel = localStorage.getItem("valorpiel");
@@ -14,9 +39,9 @@ window.addEventListener("DOMContentLoaded", () => {
   valoranteojos = localStorage.getItem("valoranteojos");
   ratio = localStorage.getItem ("ratio");
 
-  document.querySelector(`.colorcitos img[data-valor="${valorpiel}"]`).classList.add("visible");
-  document.querySelector(`.cabellos img[data-valor="${valorpelo}"]`).classList.add("visible");
-  document.querySelector(`.anteojosysol img[data-valor="${valoranteojos}"]`).classList.add("visible");
+  document.querySelector(`#colorcitos img[data-valor="${valorpiel}"]`).classList.add("visible");
+  document.querySelector(`#cabellos img[data-valor="${valorpelo}"]`).classList.add("visible");
+  document.querySelector(`#anteojosysol img[data-valor="${valoranteojos}"]`).classList.add("visible");
 
   actualizarDatosUsuario();
   actualizarAvatar();
@@ -69,6 +94,17 @@ function datosUsuario() {
         actualizarDatosUsuario();
         actualizarAvatar();
     }
+)
+  postEvent ("trofeos", {
+    NOMBREniño: nombre,
+    APELLIDOniño: apellido
+  },
+  function (data) {
+console.log("Valores individuales:", data.T1, data.T2, data.T3, data.T4, data.T5, data.T6);
+
+    if (!data) return;
+    mostrartrofeos (data.T1, data.T2, data.T3, data.T4, data.T5, data.T6)
+  }
 )
 }
 
