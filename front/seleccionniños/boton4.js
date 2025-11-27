7cd96c32f3b53a2f0b8f60a1eb0a16059a79ba25
malerossi <malena.agostina.rossi@gmail.com>
2025-11-27 10:39:25 -0300

    vamos a presentar con el gob

--- front/seleccionniños/boton4.js
+++ front/seleccionniños/boton4.js
@@ -1,4 +1,5 @@
       connect2Server()
+     
 const cuarto1 = document.getElementById("1cuarto");
 const cuarto2 = document.getElementById("2cuarto");
 const cuarto3 = document.getElementById("3cuarto");
@@ -6,15 +7,34 @@ const cuarto4 = document.getElementById("4cuarto");
 const cuarto5 = document.getElementById("5cuarto");
 const cuarto6 = document.getElementById("6cuarto");
 const cuarto7 = document.getElementById("7cuarto");
-
+const volveratras = document.getElementById("volveratras")
 let atras4 = document.getElementById ("atras");
 let seguir4 = document.getElementById ("seguir");
 
 let indice4 = 0;
-
+volveratras.addEventListener("click", function () {
+    let apellido=localStorage.getItem ("apellidoniño");
+    let nombre=localStorage.getItem ("nombreniño");
+    postEvent("trofeos",{
+        T4: true,
+        NOMBREniño:nombre,
+  APELLIDOniño:apellido
+         },function(data){
+        console.log(data)
+      }
+      )
+    window.location.href = "seleccionniños.html";
+  });
 const imagenescuarto = document.querySelectorAll (".imagenescuarto img");
-
+   atras4.style.display= "none"
+        cuarto2.style.display= "none"
+    cuarto3.style.display= "none"
+    cuarto4.style.display= "none"
+    cuarto5.style.display= "none"
+    cuarto6.style.display= "none"
+    cuarto7.style.display= "none"
 function paginasmostrar () {
+    
     imagenescuarto.forEach((pagina, i) => {
         pagina.style.display = (i === indice4) ? `block` : `none`;
     });
@@ -31,6 +51,7 @@ function paginasmostrar () {
     }
     else {
         seguir4.style.display = "block"
+     
     }
 }
 
@@ -41,20 +62,11 @@ function paginasmostrar () {
 })
 
     seguir4.addEventListener ("click", () => {
+   
     indice4 ++;
     if (indice4 > imagenescuarto.length) indice4 = imagenescuarto.length - 1;
     paginasmostrar ();
 })
-let apellido=localStorage.getItem ("apellidoniño");
-let nombre=localStorage.getItem ("nombreniño");
 
-      postEvent("trofeos",{
-      T6: true,
-      NOMBREniño:nombre,
-APELLIDOniño:apellido
-       },function(data){
-      console.log(data)
-    }
-    )
 
-paginasmostrar ();
+
